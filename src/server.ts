53ccfde57c7472fac41b91f6546c63e74315a154
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-10-18 19:13:49 -0300

    Criando o Middleware de Tratamento de Exceções

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@
 import express from "express";
 
 import { routes } from "./routes/index";
+import { errorHandling } from "./middlewares/error-handling";
 
 const PORT = 3333;
 const app = express(); // Cria a aplicação HTTP.
 
 app.use(express.json()); // Habilita o parsing de JSON no corpo das requisições.
 app.use(routes);
+
+app.use(errorHandling);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

b07d66754d1cd98ec435a3b521aca2f409be5996
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-11-01 17:05:15 -0300

    criando os métodos post, put e delete para as mesas

--- src/controllers/tables-controller.ts
+++ src/controllers/tables-controller.ts
@@ -16,11 +16,82 @@ class TableController {
     }
   }
 
-  async create(request: Request, response: Response, next: NextFunction) {}
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_number: z
+          .number({ required_error: "price is required!" })
+          .gt(0, { message: "value must be greater than 0" }),
+      });
 
-  async update(request: Request, response: Response, next: NextFunction) {}
+      const { table_number } = bodySchema.parse(request.body);
 
-  async remove(request: Request, response: Response, next: NextFunction) {}
+      await knex<TableRepository>("tables").insert({ table_number });
+
+      return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id);
+
+      const table = await knex<TableRepository>("tables")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!table) {
+        throw new AppError("table not found");
+      }
+
+      const bodySchema = z.object({
+        table_number: z
+          .number({ required_error: "price is required!" })
+          .gt(0, { message: "value must be greater than 0" }),
+      });
+
+      const { table_number } = bodySchema.parse(request.body);
+
+      await knex<TableRepository>("tables")
+        .update({ table_number, updated_at: knex.fn.now() })
+        .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id);
+
+      const table = await knex<TableRepository>("tables")
+        .select()
+        .where({ id })
+        .first();
+
+      if (!table) {
+        throw new AppError("table not found");
+      }
+      await knex<TableRepository>("tables").delete().where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { TableController };

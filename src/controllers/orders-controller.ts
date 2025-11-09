6766666d86539c747e2a2e846e177923723d90d6
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-11-08 23:10:00 -0300

    configurando o insert para salvar pedidos no banco de dados

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -43,7 +43,14 @@ class OrderController {
         throw new AppError("product not found");
       }
 
-      return response.status(201).json(product);
+      await knex<OrderRepository>("orders").insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price,
+      });
+
+      return response.status(201).json();
     } catch (error) {
       next(error);
     }
